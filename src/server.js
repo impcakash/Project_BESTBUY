b43addcd915ed82df40ae45772b6288de0abe1b5
impcakash <impcakash@gmail.com>
2022-01-21 20:25:38 +0530

    index updated

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const app = require("./index");
+
+const connect = require("./configs/db");
+
+app.listen(2345, async () => {
+  try {
+    await connect();
+    console.log("listening on port 2345");
+  } catch (e) {
+    console.log(e.message);
+  }
+});
